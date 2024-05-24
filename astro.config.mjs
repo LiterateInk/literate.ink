09dc9ba3553252e7b193e2e224ec48bb7620dbfb
Mikkel RINGAUD <mikkel@milescode.dev>
2024-05-24 08:36:56 +0200

    feat: add multiple docs support and implement pawnote

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,23 +4,53 @@ import vercel from "@astrojs/vercel/serverless";
 
 export default defineConfig({
   integrations: [starlight({
-    title: 'Documentation',
+    title: 'Literate',
     social: {
-      // GitHub for the app itself.
-      github: "https://github.com/LiterateInk/Literate"
+      github: "https://github.com/LiterateInk"
     },
-    sidebar: [{
-      label: 'Guides',
-      items: [{
-        label: 'Example Guide',
-        link: '/docs/guides/example/'
-      }]
-    }, {
-      label: 'Reference',
-      autogenerate: {
-        directory: 'reference'
+
+    components: {
+      Sidebar: "./src/components/Sidebar.astro",
+      SiteTitle: "./src/components/SiteTitle.astro",
+    },
+
+    sidebar: [
+      // Main documentation entries.
+      {
+        label: 'Guides',
+        
+        items: [
+          {
+            label: 'Example Guide',
+            link: '/docs/guides/example/'
+          }
+        ]
+      },
+      
+      // Pawnote documentation entries
+      {
+        label: 'Installation',
+        link: '/pawnote/installation'
+      },
+      {
+        label: 'Guides',
+        autogenerate: {
+          directory: 'pawnote/guides',
+        }
+      },
+      {
+        label: 'Tips and tricks: JS/TS',
+        autogenerate: {
+          directory: 'pawnote/js-tips',
+        }
+      },
+      {
+        label: 'Pronote Internals',
+        autogenerate: {
+          directory: 'pawnote/pronote-internals',
+        }
       }
-    }]
+    ]
   })],
 
   output: "server",
--- src/pages/pawnote.ts
+++
@@ -1,5 +0,0 @@
-import type { APIRoute } from "astro";
-
-export const GET: APIRoute = async ({ redirect }) => {
-  return redirect('https://pawnote.js.org', 301);
-}
