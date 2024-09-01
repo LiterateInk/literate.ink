f4b05bbf276e5b4344a35d709a31b42adc6d0e1d
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-09-01 22:18:54 +0200

    chore: add unocss

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
+
 import solid from 'vite-plugin-solid'
+import unocss from "unocss/vite";
 
 export default defineConfig({
-  plugins: [solid()],
+  plugins: [unocss(), solid()],
 })
