95f4272d6ea748c8a810f2e257b27e09071c191e
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-09-02 11:07:32 +0200

    chore: add custom icons

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,18 @@ import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
 import unocss from "unocss/vite";
 
+import icons from "unplugin-icons/vite"
+import { FileSystemIconLoader } from 'unplugin-icons/loaders'
+
 export default defineConfig({
-  plugins: [unocss(), solid()],
+  plugins: [
+    icons({
+      compiler: "solid",
+      customCollections: {
+        "literate": FileSystemIconLoader("./src/assets/icons")
+      }
+    }),
+    unocss(),
+    solid()
+  ],
 })
