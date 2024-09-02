44bf2a4c05fb679502c5e47cbb172800d419e42c
Mikkel ALMONTE--RINGAUD <vexitofficial@gmail.com>
2024-09-02 11:07:57 +0200

    feat: add color to homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,7 +2,7 @@ import { type Component } from "solid-js";
 
 const HomePage: Component = () => {
   return (
-    <div>
+    <div class="p-4">
       <p>Yes, we're currently bulding the website and the only thing done is the cute animation you saw.</p>
       <p>Please check <a href="https://literate.ink/github">our GitHub</a> in the meantime!</p>
       <p>Or <a href="https://literate.ink/discord">join our Discord server</a>!</p>
